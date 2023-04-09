a7e000d6207cd5568cae2ae94b1f7bbc2f53201a
Uallesson Nunes Ivo <uallessons@gmail.com>
2023-04-09 20:16:49 -0300

    Day 5 -- Done

---
+++ Algorithms/MiddleOfLinkedList.cs
@@ -0,0 +1,51 @@
+﻿namespace Algorithms;
+
+/*
+ * Middle of the Linked List
+ *
+ 
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+
+Example 2:
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ */
+
+internal class MiddleOfLinkedList
+{
+    public ListNode MiddleNode(ListNode head)
+    {
+        var slow = head;
+        var fast = head;
+
+        while (fast?.next != null)
+        {
+            slow = slow?.next;
+            fast = fast.next?.next;
+        }
+
+        return slow;
+    }
+
+    public class ListNode
+    {
+        public ListNode next;
+        public int val;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ Algorithms/RemoveNthNodeFromList.cs
@@ -0,0 +1,63 @@
+﻿namespace Algorithms;
+
+/*
+ * Remove Nth Node From End of List
+ *
+ 
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+ 
+
+Example 1:
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Example 2:
+Input: head = [1], n = 1
+Output: []
+
+Example 3:
+Input: head = [1,2], n = 1
+Output: [1]
+ */
+
+internal class RemoveNthNodeFromList
+{
+    public ListNode? RemoveNthFromEnd(ListNode head, int n)
+    {
+        var dummy = new ListNode(0)
+        {
+            next = head
+        };
+
+        var slow = dummy;
+        var fast = dummy;
+
+        for (var i = 0; i < n + 1; i++)
+        {
+            fast = fast?.next;
+        }
+
+        while (fast != null)
+        {
+            slow = slow?.next;
+            fast = fast?.next;
+        }
+
+        if (slow != null) slow.next = slow?.next?.next;
+
+        return dummy?.next;
+    }
+
+    public class ListNode
+    {
+        public ListNode? next;
+        public int val;
+
+        public ListNode(int val = 0, ListNode? next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
